1a7dbacd1161d23ceee8867f55c2cebcfbc1dfa5
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-07-08 21:44:35 +0700

    задача№39

---
+++ task#39/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] array = new double[10];
+  for (int i = 0; i < array.Length; i++ )
+  {
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+  }
+
+double max = array[0];
+double min = array[0];
+
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (max < array[i])
+    {
+      max = array[i];
+    }
+        if (min > array[i])
+    {
+      min = array[i];
+    }
+  }
+
+  double result = max - min;
+
+  Console.WriteLine($"\nразница между максимальным ({max}) и минимальным({min})числами: {result}");
