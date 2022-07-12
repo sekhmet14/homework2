b8a093ad5532e63218efc3b2c44be4cbb487c58a
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-07-12 13:27:17 +0700

    задача № 41

---
+++ task#41/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write($"Введи число (количество чисел): ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[num];
+
+void InputNumbers(int num){
+for (int i = 0; i < num; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int simile(int[] arrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNumbers.Length; i++)
+  {
+        if(arrayNumbers[i] > 0)
+
+
+        count++;
+    }
+  return count;
+}
+
+InputNumbers(num);
+
+Console.WriteLine($"Введено чисел больше 0: {simile(arrayNumbers)} ");
