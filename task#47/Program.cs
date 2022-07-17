4d9964ac1ce7747f9f096d19dd62e0dca2216603
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-07-17 21:18:14 +0700

    задача47

--- task#47/Program.cs
+++ task#47/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+
+
+void FillArrayRandomNumbers(double[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+ for (int j = 0; j < array.GetLength(1); j++)
+        {
+ array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+ Console.Write("[ ");
+ for (int j = 0; j < array.GetLength(1); j++)
+        {
+ Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
